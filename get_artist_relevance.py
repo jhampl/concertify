3f73f574c2e54c8abccaccac56d2f783c82a11e0
nasty.taxi <joshuahampl@gmail.com>
2024-01-25 19:38:45 +0000

    fix artist relevance scraping

--- get_artist_relevance.py
+++ get_artist_relevance.py
@@ -3,14 +3,17 @@ import requests
 import re
 
 def get_artist_relevance(artist_name):
-    # TODO more sources of relevance? active touring? band disbanded?
 
     url = 'https://www.last.fm/music/' + artist_name.replace(' ', '+')
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    metadata = soup.find('span', class_='metadata_column')
-    metadata = metadata.find_all('catalogue-metadata-heading')
+    
+    metadata = soup.find_all('dt', attrs={'class':'catalogue-metadata-heading'})
     for heading in metadata:
-        if heading.text == 'Died':
+        if heading.text.lower() == 'died':
+            return False
+        if heading.text.lower() == 'years active':
             return False
     return True
+
+print(get_artist_relevance('The Beatles'))
